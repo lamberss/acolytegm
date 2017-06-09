f4585692a557baac00cc56f70fbc2bf602f5b2c8
Steven E. Lamberson, Jr <steven.lamberson@gmail.com>
2017-06-08 22:25:46 -0500

    Change how requirements are handled.

--- setup.py
+++ setup.py
@@ -5,9 +5,21 @@ def readme():
         return f.read()
 
 setup(
+    author='Steven E. Lamberson, Jr.',
+    author_email='steven.lamberson@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Topic :: Games/Entertainment :: Role-Playing'
+    ],
+    description='A tool to help tabletop RPG game masters plan and run their games.',
+    install_requires=['pip-tools'],
+    license='Apache Software License v2.0',
+    long_description=readme(),
     name='acolytegm',
-    version='0.1dev',
     packages=['acolytegm',],
-    license='Apache 2.0',
-    long_description=readme()
+    url='https://github.com/lamberss/acolytegm',
+    version='0.1.0.dev2'
 )
